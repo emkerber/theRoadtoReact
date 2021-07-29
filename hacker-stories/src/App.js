6b6e1d689579709b6d23b0bfc1d535002a6fba85
Liz Kerber <emkerber@outlook.com>
2021-07-28 23:36:43 -0500

    Intro to JSX

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,30 +1,19 @@
-// import logo from './logo.svg';
 // import './App.css';
 
-const title = 'React';
+function getTitle(title) {
+  return title;
+}
 
 function App() {
 
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Hello {getTitle('React')}</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
     </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+
   );
 }
 
